ee3fa76384b74f08333dc00d4c01c758ee06ce12
Nehal Bhandari <nehalvbhandari@gmail.com>
2021-02-21 13:34:45 +0530

    make it better 2.0

---
+++ config/googleConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+    clientID: '169619143642-7dso7vc8qvkja039s2u2di631343kguu.apps.googleusercontent.com',
+    clientSecret: 'SiajT2g2JenUB9iw_kO-jleb',
+    callbackURL: "http://localhost:3000/auth/google/callback",
+    passReqToCallback : true
+}
---
+++ config/twitterConfig.js
@@ -0,0 +1,5 @@
+module.exports={
+    consumerKey: 'hWSjzoYlgiUPIpaHjB94a6gkC',
+    consumerSecret: 'IK1l2bAT6Mh9gxG3kF0Gu4oWgEV9oyNQb9exiXfFgNSs6vx887',
+    callbackURL: "http://localhost:3000/auth/twitter/callback"
+  }
