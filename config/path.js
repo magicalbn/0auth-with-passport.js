814a31d646e78323a80c0750810b15c446fdce18
Nehal Bhandari <nehalvbhandari@gmail.com>
2023-08-25 14:02:21 +0530

    config path update

--- config/path.js
+++ config/path.js
@@ -1,7 +1,7 @@
 
 const dev = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || 3000;
-const path = dev?`http://localhost:${port}`:'https://passportjs-0auth.herokuapp.com'
+const path = dev?`http://localhost:${port}`:'https://passportjs-0auth.onrender.com'
 
 module.exports = path;
  
